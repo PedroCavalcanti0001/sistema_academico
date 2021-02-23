b270ac0f16fb7171bfb46997639beb8a9e316074
Pedro_NTI <pedroeugeniocavalcanti@gmail.com>
2021-02-23 18:19:29 -0300

    tela de login criada

---
+++ lib/view/register.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class RegisterView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(title:  Center(child: Text("Registrar-se"),),),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+
+          ),
+        ),
+      ),
+    );
+  }
+}
