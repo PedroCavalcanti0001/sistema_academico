9adcec9c9d7c2938ea30071d537eb4167920759f
Pedro_NTI <pedroeugeniocavalcanti@gmail.com>
2021-02-24 17:50:01 -0300

    agora o Teacher extende a Person

--- lib/model/teacher.dart
+++ lib/model/teacher.dart
@@ -1,5 +1,17 @@
 import 'package:sistema_academico/model/person.dart';
 
-abstract class Teacher implements Person{
-    List<String> _subjects;
+class Teacher extends Person {
+  /*
+    Materias do professor
+   */
+  List<String> _subjects = List();
+
+  List<String> get subjects => _subjects;
+
+  set subjects(List<String> value) {
+    _subjects = value;
+  }
+
+  Teacher(int id, String name, String lastName, String cpf, String age, String userName) : super(id, name, lastName, cpf, age, userName);
+
 }
