6ad2ba7f5df42b1c9337fdecc56cde321fffd26e
Pedro_NTI <pedroeugeniocavalcanti@gmail.com>
2021-02-26 18:44:52 -0300

    propriedade userType adicionada

--- lib/controller/register_provider.dart
+++ lib/controller/register_provider.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:sistema_academico/helpers/enums/user_type.dart';
 
 class RegisterProvider extends ChangeNotifier{
+  UserType _userType;
 
+  UserType get userType => _userType;
 
+  set userType(UserType value) {
+    _userType = value;
+  }
 }
